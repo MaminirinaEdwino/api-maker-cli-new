079cb35ae3d789c1a681734b314965db58eb2df6
Maminirina Edwino <edwinomaminirina@gmail.com>
2025-12-03 22:20:05 +0300

    utils: use template for showing cli help

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"github/mameinirinaedwino/api-maker-cli/module/apimaker"
+	"github/mameinirinaedwino/api-maker-cli/module/utils"
 	colortext "github.com/MaminirinaEdwino/colorText"
-	"github.com/rivo/tview"
 )
 
 func main() {
@@ -13,7 +13,6 @@ func main() {
 
 	createProject := flag.Bool("create_project", false, "The command that create a new project Config")
 	generateProject := flag.Bool("generate_project", false, "Generate the project from the conf file")
-	showBox := flag.Bool("showBox", false, "Show a teste")
 	flag.Parse()
 
 	switch {
@@ -22,17 +21,8 @@ func main() {
 		apimaker.CreateProject()
 	case *generateProject:
 		fmt.Println("Generate Project")
-	case *showBox:
-		box := tview.NewBox().SetBorder(true).SetTitle("Hello world")
-		if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-			panic(err)
-		}
 	default:
-		fmt.Println(`
-Use these command : 
-	- create_project : To create a new Project config
-	- generate_project : To generate the code of the project 
-		`)
+		utils.ShowCliDocumentation()
 	}
 
 }
