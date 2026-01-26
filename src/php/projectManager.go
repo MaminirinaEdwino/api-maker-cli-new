129c2d1cc2871e23adb0bdfb595e60a88468384e
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-01-26 13:04:02 +0300

    add: php project structure

---
+++ src/php/projectManager.go
@@ -0,0 +1,13 @@
+package php
+
+import "fmt"
+
+func CreateWebAppProject(){
+	var projectName string
+	var Dbname string
+	var model []string
+	var pages []string
+	var components []string
+	fmt.Println("Create a PHP WEB App")
+
+}
--- src/php/projectstructure.go
+++ src/php/projectstructure.go
@@ -1,1 +1,35 @@
 package php
+
+import (
+	"github/MaminirinaEdwino/api-maker-cli/src/utils"
+	"os"
+)
+
+func CreateProjectStructure(Projectname string){
+	err := os.MkdirAll(Projectname+"/asset/style", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/asset/script", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/asset/image", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/auth", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/components", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/config", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/controllers", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/core", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/models", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/repositories", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/router", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/templaterenderer", os.ModePerm)
+	utils.ErrorChecker(err)
+	err = os.MkdirAll(Projectname+"/src/views", os.ModePerm)
+	utils.ErrorChecker(err)
+}
