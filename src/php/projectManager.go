fc9dd579892c4f88c00c47519286673e73c81423
Maminirina Edwino <edwinomainirina@gmail.com>
2026-02-18 13:02:31 +0300

    implement: setProjectName and setDbname function, project manager basic function

--- src/php/projectManager.go
+++ src/php/projectManager.go
@@ -2,35 +2,76 @@ package php
 
 import "fmt"
 
-
-func getModels(){
-
-}
-
-func getPages(){
+func getModels() {
 
 }
 
-func getComponents(){
-
-} 
-
-func getDbname(){
+func getPages() {
 
 }
 
-func getProjectName(){
-	
+func getComponents() {
+
 }
 
-func CreateWebAppProject(){
+func createComponents() {
+
+}
+
+func createModel() {
+
+}
+
+func setDbname() string {
+	dbName := ""
+	for dbName == ""{
+		fmt.Print("Entrer le nom de la base de donnée : ")
+		fmt.Scanln(&dbName)
+	}
+	return dbName
+}
+
+func setProjectName() string {
+	projectName := ""
+	for projectName == ""{
+		fmt.Printf("Entrer le nom du projet : ")
+		fmt.Scanln(&projectName)
+	}
+	return projectName
+}
+
+func getAction() string {
+	cmd := ""
+	for cmd == "" {
+		fmt.Print("Entrer une commande : ")
+		fmt.Scanln(&cmd)
+	}
+	return cmd
+}
+
+func CreateWebAppProject() {
 	var projectName string
 	var Dbname string
 	var model []string
 	var pages []string
 	var components []string
-	var model []string 
+	var model []string
 
 	fmt.Println("Create a PHP WEB App")
-
+	action := ""
+	for action != getAction() {
+		switch action {
+		case "project_name":
+			projectName = setProjectName()
+		case "db_name":
+			Dbname = setDbname() 
+		case "new_component":
+		case "delete_component":
+		case "new_page":
+		case "new_model":
+		case "delete_model":
+		case "quit":
+			break
+		}
+	}
 }
