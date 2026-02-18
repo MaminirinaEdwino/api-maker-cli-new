33fc030b9aceaf97079a999aedf7a697e348e826
Maminirina Edwino <edwinomainirina@gmail.com>
2026-02-18 16:05:02 +0300

    implement: fileUploader basic function

--- src/php/projectstructure.go
+++ src/php/projectstructure.go
@@ -25,6 +25,44 @@ function To_relative_path($route ){
 ?>
 	`)
 }
+
+func CreateFileUploaderFile(projectName string){
+	file, err := os.Create(projectName+"/src/core/fileUploader.php")
+	utils.ErrorChecker(err)
+	file.WriteString(`
+<?php
+function FileUploader(string $file_name): string
+{
+    if (
+        isset($_FILES[$file_name]) and $_FILES[$file_name]['error'] == 0
+    ) {
+        if ($_FILES[$file_name]['size'] <= 1000000) {
+            $infosfichier =
+                pathinfo($_FILES[$file_name]['name']);
+            $extension_upload = $infosfichier['extension'];
+            $extensions_autorisees = array(
+                'jpg',
+                'jpeg',
+                'gif',
+                'png'
+            );
+            if (in_array(
+                $extension_upload,
+                $extensions_autorisees
+            )) {
+                move_uploaded_file($_FILES[$file_name]['tmp_name'], 'uploads/' .
+                    basename($_FILES[$file_name]['name']));
+                echo "L'envoi a bien été effectué !";
+                return 'uploads/' .
+                    basename($_FILES[$file_name]['name']);
+            }
+        }
+    }
+    return "";
+}
+	`)
+}
+
 func CreateProjectStructure(Projectname string){
 	err := os.MkdirAll(Projectname+"/asset/style", os.ModePerm)
 	utils.ErrorChecker(err)
